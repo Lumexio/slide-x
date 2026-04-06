ad55188d7a592269a16f35d7227da4e69cf5186b
lumexio <trotroyu@gmail.com>
2026-04-06 02:38:05 -0600

    feat: New character menu animations

--- gui/CSGSphere.gd
+++ gui/CSGSphere.gd
@@ -12,5 +12,4 @@ func _ready():
 
 func _physics_process(delta: float)-> void:
 	$".".rotate_y(.5 * delta)
-
 	
--- gui/menu_character.gd
+++ gui/menu_character.gd
@@ -5,27 +5,79 @@ extends Spatial
 # var a = 2
 # var b = "text"
 
+onready var KineticChad=$"KineticChad"
+onready var AkimboBoy=$"AkimboBoy"
+onready var FairyFire=$"FairyFire"
+var selection_tween = Tween.new()
+var current_character = ""
+
+const CHARACTER_ORDER = ["AkimboBoy", "KineticChad", "FairyFire"]
+onready var CHARACTER_NODES = [AkimboBoy, KineticChad, FairyFire]
+const CHARACTER_SPACING = 3.0
+const CHARACTER_MOVE_TIME = 0.35
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	add_child(selection_tween)
+	_select_character("AkimboBoy", false)
+	
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+# Set initial positions for characters
 
 
-func _on_Control_pressed():
-	get_tree().change_scene("res://scenes/levels/level_1.tscn")
+# I want that the characters are next to each other and when I click on the button assigned	to the character, it moves to the center and the other one moves to the side. I also want that when I click on the button assigned to the character, it changes the current_character variable to the name of the character.
 
 
+func _select_character(character_name, animate = true):
+	current_character = character_name
+
+	var selected_index = CHARACTER_ORDER.find(character_name)
+	if selected_index == -1:
+		return
+
+	selection_tween.stop_all()
+	selection_tween.remove_all()
+
+	for index in range(CHARACTER_NODES.size()):
+		var character = CHARACTER_NODES[index]
+		var distance_from_selected = index - selected_index
+		var target_x = distance_from_selected * CHARACTER_SPACING
+		var target_translation = Vector3(target_x, character.translation.y, character.translation.z)
+
+		if animate:
+			selection_tween.interpolate_property(
+				character,
+				"translation",
+				character.translation,
+				target_translation,
+				CHARACTER_MOVE_TIME,
+				Tween.TRANS_QUAD,
+				Tween.EASE_OUT
+			)
+		else:
+			character.translation = target_translation
+
+	if animate:
+		selection_tween.start()
 
 
+func _on_AkimboBoy_pressed():
+	_select_character("AkimboBoy")
 
 func _on_Kinetic_Chad_pressed():
-	get_tree().change_scene("res://scenes/levels/level_1.tscn")
+	_select_character("KineticChad")
+
+func _on_FairyFire_pressed():
+	_select_character("FairyFire")
 
 
 func _on__Back_pressed():
 	get_tree().change_scene("res://gui/main_menu.tscn")
+
+func _on_Start_Game_pressed():
+	get_tree().change_scene("res://scenes/levels/level_1.tscn")
